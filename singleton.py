939c3d08d92f4fd3002a07ec779bede4b949f7a6
virocha11 <vivianadrianarocha@gmail.com>
2024-08-11 22:11:29 -0300

    adição do singleton

---
+++ singleton.py
@@ -0,0 +1,15 @@
+# código retirado do site GeeksforGeeks
+
+class Singleton(object):
+    def __new__(cls):
+        if not hasattr(cls, '__instance'):
+            cls.__instance = super(Singleton, cls).__new__(cls)
+        return cls.__instance
+
+singleton = Singleton()
+new_singleton = Singleton()
+
+print(singleton is new_singleton)
+
+singleton.singl_variable = "Singleton Variable"
+print(new_singleton.singl_variable)
